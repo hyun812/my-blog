fe064e608e26a120fca476443ba70519d7f76c74
이승현 <sh980812@naver.com>
2024-12-29 19:55:12 +0900

    feat #11: CategoryChips 컴포넌트 구현 - handleCategoryClick: categoryName을 전달받아 routing 처리

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,17 @@
+import CategoryChips from '@/components/posts/CategoryChips';
 import PostList from '@/components/posts/PostList';
+import { getPostList, getPostListCategory } from '@/utils/post';
 
-const PostsPage = () => {
-  return <PostList />;
+const PostsPage = async () => {
+  const category = getPostListCategory();
+  const postList = await getPostList();
+
+  return (
+    <>
+      <CategoryChips category={category} />
+      <PostList postList={postList} />
+    </>
+  );
 };
 
 export default PostsPage;
