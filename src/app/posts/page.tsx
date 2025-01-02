7a22854735a1ecc516a476a60abdd25a3b902ad5
이승현 <sh980812@naver.com>
2025-01-02 15:18:35 +0900

    refactor #17: 전반적인 UI 수정

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -8,6 +8,10 @@ const PostsPage = async () => {
 
   return (
     <>
+      <div className="my-10">
+        <h1 className="text-3xl font-bold pb-3">Posts</h1>
+        <p>전체 포스트 목록을 확인해보세요</p>
+      </div>
       <CategoryChips category={category} />
       <PostList postList={postList} />
     </>
