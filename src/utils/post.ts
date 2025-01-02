0d3e36e7778f80e15f9cec154b57f879eaf5ea16
이승현 <sh980812@naver.com>
2025-01-02 17:05:27 +0900

    refactor #17: post util을 snippet에서 함께 사용할 수 있도록 리팩토링

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -4,22 +4,27 @@ import { sync } from 'glob';
 import matter from 'gray-matter';
 import path from 'path';
 
-const BASE_PATH = 'public/posts';
-const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
+export type ContentType = 'posts' | 'snippets';
 
-// 모든 MDX 파일 조회
-export const getPostPaths = (category?: string): string[] => {
-  const folder = category || '**';
-  const paths: string[] = sync(`${POSTS_PATH}/${folder}/**/*.mdx`);
+const BASE_POSTS_PATH = 'public/posts';
+const BASE_SNIPPET_PATH = 'public/snippets';
+
+const POSTS_PATH = path.join(process.cwd(), BASE_POSTS_PATH);
+const SNIPPET_PATH = path.join(process.cwd(), BASE_SNIPPET_PATH);
+
+// 콘텐츠에 맞는 MDX 파일 경로 조회
+export const getMDXFilePaths = (content: ContentType) => {
+  const CONTENT_PATH = content === 'posts' ? POSTS_PATH + '/**' : SNIPPET_PATH;
+  const paths: string[] = sync(`${CONTENT_PATH}/**/*.mdx`);
 
   return paths;
 };
 
-// 모든 포스트 목록 조회
-export const getPostList = async (category?: string): Promise<Post[]> => {
-  const paths = getPostPaths(category);
-  const posts = await Promise.all(paths.map((path) => parsePost(path)));
+// 콘텐츠에 맞는 MDX 파일 목록 조회
+export const getMDXFileList = async (content: ContentType) => {
+  const paths = getMDXFilePaths(content);
 
+  const posts = await Promise.all(paths.map((path) => parsePost(path)));
   return posts;
 };
 
@@ -36,6 +41,14 @@ export const parsePost = (postPath: string): Post => {
 // category와 fileName을 받아 해당 포스트 조회
 export const getPostDetail = (category: string, fileName: string) => {
   const path = `${POSTS_PATH}/${category}/${fileName}.mdx`;
+  const post = parsePost(path);
+
+  return post;
+};
+
+// category와 fileName을 받아 해당 포스트 조회
+export const getSnippetDetail = (fileName: string) => {
+  const path = `${SNIPPET_PATH}/${fileName}.mdx`;
   const post = parsePost(path);
 
   return post;
@@ -75,7 +88,7 @@ export const parseContents = (source: string): ITableOfContent[] => {
 
 // 모든 포스트 카테고리 조회
 export const getPostListCategory = (): string[] => {
-  const category = sync(`${BASE_PATH}/**/`)
+  const category = sync(`${BASE_POSTS_PATH}/**/`)
     .map((folderPaths) => path.basename(folderPaths))
     .filter((path) => path !== 'posts');
 
