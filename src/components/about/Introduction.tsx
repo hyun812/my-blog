56122740a4807b65284588625f00920c500bdcf8
이승현 <sh980812@naver.com>
2025-01-07 18:02:22 +0900

    feat #19: Introduction 컴포넌트 구현

---
+++ src/components/about/Introduction.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import ExternalLink from '../common/ExternalLink';
+import { ICONS } from '@/constants/Icons';
+import { introductions } from '@/constants/about';
+
+const Introduction = () => {
+  return (
+    <>
+      <div className="flex-center gap-10 mt-20">
+        <div>
+          <div>
+            <h2 className="text-3xl">
+              안녕하세요 <br /> <span className="text-4xl font-bold pr-1">이승현</span>입니다.
+            </h2>
+          </div>
+          <div className="flex pt-3">
+            <ExternalLink href="https://github.com/hyun812">{ICONS.GITHUB}</ExternalLink>
+            <ExternalLink href="https://www.instagram.com/hyun_ili">{ICONS.INSTAGRAM}</ExternalLink>
+            <ExternalLink href="mailto:dltmdgus4802@gmail.com">{ICONS.MAIL}</ExternalLink>
+          </div>
+        </div>
+        <Image
+          src="/profile.png"
+          width={140}
+          height={140}
+          alt="profile"
+          className="object-fill rounded-full"
+        />
+      </div>
+      <ul className="mt-20 flex-1 flex flex-col gap-5">
+        {introductions.map((intro, index) => (
+          <li key={index}>{intro}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Introduction;
