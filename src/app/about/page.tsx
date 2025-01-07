337df55b1d8a96d07d704d012ce1563680cd6647
이승현 <sh980812@naver.com>
2025-01-07 18:04:45 +0900

    feat #19: Award 컴포넌트 구현

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,21 @@
+import Activity from '@/components/about/Activity';
+import Award from '@/components/about/Award';
+import Introduction from '@/components/about/Introduction';
+import Project from '@/components/about/Project';
+
 const AboutPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="my-10">
+        <h1 className="text-3xl font-bold pb-3">About</h1>
+        <p>저에 대해 소개합니다.</p>
+      </div>
+      <Introduction />
+      <Project />
+      <Activity />
+      <Award />
+    </>
+  );
 };
 
 export default AboutPage;
---
+++ src/components/about/Award.tsx
@@ -0,0 +1,27 @@
+import { awards } from '@/constants/about';
+
+const Award = () => {
+  return (
+    <div className="mt-20">
+      <h2 className="text-3xl font-bold">자격증 & 수상</h2>
+      <div className="mt-10">
+        <ul className="flex flex-col gap-10">
+          {awards.map((award) => (
+            <li
+              key={award.name}
+              className="flex flex-col sm:flex-row gap-5"
+            >
+              <div className="basis-2/5">
+                <p className="text-lg font-semibold">{award.name}</p>
+                <p className="text-sm text-neutral-500 pt-1">{award.date}</p>
+              </div>
+              <p className="flex-1">{award.descriptions}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Award;
