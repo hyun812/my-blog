441d54a97fc55ecf2770fdbbc1c2a8ce461fe708
이승현 <sh980812@naver.com>
2025-01-18 17:21:17 +0900

    fix #33: button이 보여지는 부분보다 click 범위가 더 넓은 이슈 해결

--- src/components/home/MorePostButton.tsx
+++ src/components/home/MorePostButton.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 
 const MorePostButton = () => {
   return (
-    <Link href="/posts">
-      <div className="mx-auto mt-5 flex-center text-center w-3/5 h-12 bg-primary-400 pointerHover:hover:bg-primary-500 text-white rounded-xl font-semibold">
-        <p>모든 글 보기</p>
-      </div>
+    <Link
+      className="mx-auto mt-5 flex-center text-center w-full lg:w-3/5 h-12 bg-primary-400 pointerHover:hover:bg-primary-500 text-white rounded-xl font-semibold"
+      href="/posts"
+    >
+      <p>모든 글 보기</p>
     </Link>
   );
 };
