68f892f1cbeddda9225f010cd5eac6f1ec0fb130
이승현 <sh980812@naver.com>
2025-01-18 17:03:56 +0900

    fix #33: DropDownNavigaion blur 처리 안되는 이슈 해결

--- src/components/common/DropDownNavigation.tsx
+++ src/components/common/DropDownNavigation.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 const DropDownNavigation = () => {
   return (
-    <div className="w-full h-36 absolute top-full left-0 right-0 z-40 glassmorphism">
-      <div className="flex flex-col gap-1 px-6 py-2 border-t border-b font-semibold">
+    <div className="w-full h-36 absolute top-full left-0 right-0 z-40">
+      <div className="glassmorphism flex flex-col gap-1 px-6 py-2 border-t border-b font-semibold">
         {menus.slice(1).map((menu) => (
           <Link
             className="rounded-lg p-2 icon-hover"
