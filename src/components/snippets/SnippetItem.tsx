8de64309065dec7bac73d3de46b84e7758e2a706
이승현 <sh980812@naver.com>
2025-01-25 16:42:02 +0900

    refactor #42: ul 태그 하위에 li 태그가 오도록 수정

--- src/components/snippets/SnippetItem.tsx
+++ src/components/snippets/SnippetItem.tsx
@@ -9,15 +9,12 @@ const SnippetItem = ({ snippet }: SnippetItemProps) => {
   const { title, date, fileName } = snippet.data;
 
   return (
-    <Link
-      key={date}
-      href={`/snippets/${fileName}`}
-    >
-      <li className="border rounded-lg pointerHover:hover:bg-basic-100 cursor-pointer dark:pointerHover:hover:bg-basic-800 p-5">
+    <li className="border rounded-lg pointerHover:hover:bg-basic-100 cursor-pointer dark:pointerHover:hover:bg-basic-800 p-5">
+      <Link href={`/snippets/${fileName}`}>
         <h2 className="font-bold pb-1 line-clamp-1">{title}</h2>
         <p className="text-sm text-basic-400">{date}</p>
-      </li>
-    </Link>
+      </Link>
+    </li>
   );
 };
 
