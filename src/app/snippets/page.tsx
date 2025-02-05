b96689f10858a7c380e9fd1cd4566feca022ef86
이승현 <sh980812@naver.com>
2025-02-05 18:17:38 +0900

    style #52: snippet 페이지 padding 추가

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -7,7 +7,7 @@ const SnippetsPage = async () => {
   const snippetList = await getMDXFileList('snippets');
 
   return (
-    <div className="w-full">
+    <div className="w-full pb-24">
       <div className="my-10">
         <h1 className="text-3xl font-bold pb-3">Snippets</h1>
         <span>개발하면서 유용하게 사용했던 코드조각 모음입니다</span>
